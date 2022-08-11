3718548cad0d202f96690b7d9ce584edcbfb7a70
ziming <ziming.opensource@gmail.com>
2022-08-11 17:52:36 +0800

    add some enums and bump to php 8.1 minimum

---
+++ src/Enums/HousingTypeEnum.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ziming\LaravelMyinfoSg\Enums;
+
+// For my personal use, don't use it
+
+enum HousingTypeEnum: string
+{
+    // HDB
+    case ONE_ROOM_FLAT = '1-ROOM FLAT (HDB)';
+    case TWO_ROOM_FLAT = '2-ROOM FLAT (HDB)';
+    case THREE_ROOM_FLAT = '3-ROOM FLAT (HDB)';
+    case FOUR_ROOM_FLAT = '4-ROOM FLAT (HDB)';
+    case FIVE_ROOM_FLAT = '5-ROOM FLAT (HDB)';
+    case EXECUTIVE_FLAT = 'EXECUTIVE FLAT (HDB)';
+    case STUDIO_APARTMENT = 'STUDIO APARTMENT (HDB)';
+
+    // Private
+    case DETACHED_HOUSE = 'DETACHED HOUSE';
+    case SEMI_DETACHED_HOUSE = 'SEMI-DETACHED HOUSE';
+    case TERRACE_HOUSE = 'TERRACE HOUSE';
+    case CONDOMINIUM = 'CONDOMINIUM';
+    case EXECUTIVE_CONDOMINIUM = 'EXECUTIVE CONDOMINIUM';
+    case APARTMENT = 'APARTMENT';
+}
---
+++ src/Enums/MaritalStatusEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ziming\LaravelMyinfoSg\Enums;
+
+// For my personal use, don't use it
+
+enum MaritalStatusEnum: string
+{
+    case SINGLE = 'SINGLE';
+    case MARRIED = 'MARRIED';
+    case WIDOWED = 'WIDOWED';
+    case DIVORCED = 'DIVORCED';
+}
