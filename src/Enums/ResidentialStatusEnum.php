742410fce0dbd4de23a393ffdc92ecf720b010a6
ziming <ziming.opensource@gmail.com>
2022-09-22 20:38:44 +0800

    Add more enum types

---
+++ src/Enums/ResidentialStatusEnum.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ziming\LaravelMyinfoSg\Enums;
+
+/**
+ * For my personal use, don't use it
+ * @internal
+ */
+enum ResidentialStatusEnum: string
+{
+    case ALIEN = 'SINGLE';
+    case CITIZEN = 'CITIZEN';
+    case PR = 'PR';
+    case UNKNOWN = 'UNKNOWN';
+    case NOT_APPLICABLE = 'NOT APPLICABLE';
+}
---
+++ src/Enums/SexEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ziming\LaravelMyinfoSg\Enums;
+
+/**
+ * For my personal use, don't use it
+ * @internal
+ */
+enum SexEnum: string
+{
+    case FEMALE = 'FEMALE';
+    case MALE = 'MALE';
+    case UNKNOWN = 'UNKNOWN';
+}
