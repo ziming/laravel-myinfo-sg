dd127f240c4b272ccbe7a3ad17f549aa054d34dd
ziming <ziming.opensource@gmail.com>
2022-10-17 20:32:01 +0800

    Add HdbTypeEnum

---
+++ src/Enums/HdbTypeEnum.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Ziming\LaravelMyinfoSg\Enums;
+
+use ArchTech\Enums\Names;
+use ArchTech\Enums\Options;
+use ArchTech\Enums\Values;
+
+/**
+ * For my personal use, don't use it
+ * @internal
+ */
+enum HdbTypeEnum: string
+{
+    use Names, Values, Options;
+
+    // HDB
+    case ONE_ROOM_FLAT = '1-ROOM FLAT (HDB)';
+    case TWO_ROOM_FLAT = '2-ROOM FLAT (HDB)';
+    case THREE_ROOM_FLAT = '3-ROOM FLAT (HDB)';
+    case FOUR_ROOM_FLAT = '4-ROOM FLAT (HDB)';
+    case FIVE_ROOM_FLAT = '5-ROOM FLAT (HDB)';
+    case EXECUTIVE_FLAT = 'EXECUTIVE FLAT (HDB)';
+    case STUDIO_APARTMENT = 'STUDIO APARTMENT (HDB)';
+
+    public static function getOptions(): array
+    {
+        return array_column(self::cases(), 'value', 'value');
+    }
+}
