1618852f6cfedd8390c9e646455c0751f0ea6b47
ziming <ziming.opensource@gmail.com>
2022-10-17 20:34:50 +0800

    remove values method

--- src/Enums/HousingTypeEnum.php
+++ src/Enums/HousingTypeEnum.php
@@ -35,9 +35,4 @@ enum HousingTypeEnum: string
     {
         return array_column(self::cases(), 'value', 'value');
     }
-
-    public static function values(): array
-    {
-        return array_column(self::cases(), 'value');
-    }
 }
