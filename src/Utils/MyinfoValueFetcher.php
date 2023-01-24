8d3dd7921c967af94028e23e7ce832338026704b
ziming <ziming.opensource@gmail.com>
2023-01-25 01:14:14 +0800

    change to mobile phone full

--- src/Utils/MyinfoValueFetcher.php
+++ src/Utils/MyinfoValueFetcher.php
@@ -76,7 +76,7 @@ final class MyinfoValueFetcher
         return Arr::get($this->myinfoData, "mobileno.nbr.{$key}") ?: null;
     }
 
-    protected function fullMobilePhone(): ?string
+    protected function mobilePhoneFull(): ?string
     {
         $prefix = Arr::get($this->myinfoData, 'mobileno.prefix.value');
         $areaCode = Arr::get($this->myinfoData, 'mobileno.areacode.value');
